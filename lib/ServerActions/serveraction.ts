1e0745ece7f91a9972aa620fc97a4c094d421826
fizza897 <fizzaazam6@gmail.com>
2024-07-15 12:34:20 +0500

    lib/ServerActions/serveraction.ts

--- lib/ServerActions/serveraction.ts
+++ lib/ServerActions/serveraction.ts
@@ -1,11 +1,16 @@
 "use server"
 
 import { auth, signOut } from "@/auth"
+import { Message } from "@/models/messageModels/messsage.Models";
 import { redirect } from "next/navigation"
 // module level server actions
 export const sendMessage =async(image:string,receiverId:string,messageType:"image" | "text")=>{
     try {
-        const authUser = await auth()
+        const authUser = await auth();
+        const senderId =authUser?.user?._id
+        const newMessage =await Message.create({
+            name:"string"
+        })
     } catch (error) {
         
     }
